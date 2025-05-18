65b6c69b299c655818d56217f75ce0eabee44f6d
Sutima <me@sutima.net>
2025-05-18 21:02:48 +0200

    Regulp

--- gulpfile.js
+++ gulpfile.js
@@ -34,9 +34,9 @@ var jsFiles = [
             'app/js/guidance_profiles.js',
             'app/js/systemPanel.js',
             'app/js/tripwire/*.js',
+            'app/js/commentlist.js',
             'app/js/killBoard.js',
             'app/js/add-gate-connections.js',
-            'app/js/commentlist.js',
             'app/js/*.js',
             'app/js/**/*.js'
         ],
