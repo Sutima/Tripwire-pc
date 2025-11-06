90a2e6f1abc1f7a20b860ea102c736ba2129ce28
Astriania <singularity.syndicate.eve@gmail.com>
2025-11-06 23:02:21 +0000

    XSS - Validate mask when saving options

--- lib.inc.php
+++ lib.inc.php
@@ -32,3 +32,33 @@ function checkAdmin($mask) {
 
 	return $stmt->rowCount() == 0 ? false : true;
 }
+
+function verifyMask($mask) {
+	global $mysql;
+	
+	$checkMask = explode('.', $mask);
+	if ($checkMask[1] == 0 && $checkMask[0] != 0) {
+		// Check custom mask
+		$query = 'SELECT masks.maskID FROM masks INNER JOIN `groups` ON masks.maskID = `groups`.maskID WHERE masks.maskID = :maskID AND ((ownerID = :characterID AND ownerType = 1373) OR (ownerID = :corporationID AND ownerType = 2) OR (ownerID = :allianceID AND ownerType = 3) OR (eveID = :characterID AND eveType = 1373) OR (eveID = :corporationID AND eveType = 2) OR (eveID = :allianceID AND eveType = 3))';
+		$stmt = $mysql->prepare($query);
+		$stmt->bindValue(':characterID', $_SESSION['characterID']);
+		$stmt->bindValue(':corporationID', $_SESSION['corporationID']);
+		$stmt->bindValue(':allianceID', $_SESSION['allianceID']);
+		$stmt->bindValue(':maskID', $mask);
+
+		if ($stmt->execute() && $stmt->fetchColumn(0) != $mask) {
+			return $_SESSION['corporationID'] . '.2';
+		}
+	} else if ($checkMask[1] == 1 && $checkMask[0] != $_SESSION['characterID']) {
+		// Force current character mask
+		return $_SESSION['characterID'] . '.1';
+	} else if ($checkMask[1] == 2 && $checkMask[0] != $_SESSION['corporationID']) {
+		// Force current corporation mask
+		return $_SESSION['corporationID'] . '.2';
+	} else if ($checkMask[1] == 3 && $checkMask[0] != ($_SESSION['allianceID'] ?? false)) {
+		// Force current alliance mask (or corp if none)
+		return $_SESSION['allianceID'] ? $_SESSION['allianceID'] . '.3' : $_SESSION['corporationID'] . '.2';
+	}
+	
+	return $mask;
+}
